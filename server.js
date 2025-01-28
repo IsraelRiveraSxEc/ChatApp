8cfc8d20f4fa593726b3ed77d722eebe123b76f6
IsraelRiveraSxEc <nelsonespinosalod@gmail.com>
2025-01-28 15:12:04 -0500

    Update2

--- server.js
+++ server.js
@@ -14,6 +14,40 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Lista de usuarios conectados
+let usuarios = new Set();
+
+// Manejo de conexiones de Socket.IO
+io.on('connection', (socket) => {
+    console.log('Usuario conectado');
+
+    // Manejo de nuevo usuario
+    socket.on('nuevo usuario', (username) => {
+        socket.username = username;
+        usuarios.add(username);
+        io.emit('mensaje sistema', `${username} se ha unido al chat`);
+        io.emit('lista usuarios', Array.from(usuarios));
+    });
+
+    // Manejo de mensajes
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', {
+            usuario: socket.username,
+            mensaje: msg,
+            tiempo: new Date().toLocaleTimeString()
+        });
+    });
+
+    // Manejo de desconexión
+    socket.on('disconnect', () => {
+        if (socket.username) {
+            usuarios.delete(socket.username);
+            io.emit('mensaje sistema', `${socket.username} ha salido del chat`);
+            io.emit('lista usuarios', Array.from(usuarios));
+        }
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`Servidor iniciado en puerto ${PORT}`);
